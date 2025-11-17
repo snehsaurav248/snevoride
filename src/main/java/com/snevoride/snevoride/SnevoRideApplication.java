51806ae383b9f37ebb08a5cad69a8c50c5a4e744
Sneh Saurav <you@example.com>
2025-11-17 22:23:06 +0530

    Backend service for SnevoRide ride-hailing application

---
+++ src/main/java/com/snevoride/snevoride/SnevoRideApplication.java
@@ -0,0 +1,13 @@
+package com.snevoride.snevoride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnevoRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnevoRideApplication.class, args);
+	}
+
+}
